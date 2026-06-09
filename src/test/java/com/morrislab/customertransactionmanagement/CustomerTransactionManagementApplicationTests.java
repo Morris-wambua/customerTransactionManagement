587075e2f788ea3c6fb6634325e56374a228b2bf
morris <morriswambua7@gmail.com>
2026-06-09 06:48:03 -0700

    first commit

---
+++ src/test/java/com/morrislab/customertransactionmanagement/CustomerTransactionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morrislab.customertransactionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerTransactionManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
